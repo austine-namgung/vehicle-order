84b199c7b03c9797df8454cc405339198abdd2dd
남궁성환 <saturn203@naver.com>
2020-10-22 18:35:14 +0900

    d

--- src/main/java/com/example/order/config/KafkaProducerConfig.java
+++ src/main/java/com/example/order/config/KafkaProducerConfig.java
@@ -23,28 +23,19 @@ public class KafkaProducerConfig {
     public Map<String, Object> producerConfig() {
 
         Map<String, Object> props = new HashMap<>();
-
         // server host 지정
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, env.getProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG));
-
         // retries 횟수
         props.put(ProducerConfig.RETRIES_CONFIG, env.getProperty(ProducerConfig.RETRIES_CONFIG));
-
         // batcch size 지정
         props.put(ProducerConfig.BATCH_SIZE_CONFIG, env.getProperty(ProducerConfig.BATCH_SIZE_CONFIG));
-
         // linger.ms
         props.put(ProducerConfig.LINGER_MS_CONFIG, env.getProperty(ProducerConfig.LINGER_MS_CONFIG));
-
         // buufer memory size 지정
-        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, env.getProperty(ProducerConfig.BUFFER_MEMORY_CONFIG));
-
-        // key serialize 지정
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, env.getProperty(ProducerConfig.BUFFER_MEMORY_CONFIG));        // key serialize 지정
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-
         // value serialize 지정
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);        
         return props;
 
     }
@@ -52,7 +43,6 @@ public class KafkaProducerConfig {
     public ProducerFactory<String, String> producerFactory() {
         return new DefaultKafkaProducerFactory<>(producerConfig());
     }
-
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
